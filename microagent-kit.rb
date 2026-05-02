4ef1802109b2b038745483614344efa19edbd922
Geoff Belknap <geoffbelknap@users.noreply.github.com>
2026-05-02 10:06:16 -0700

    Skip Apple VF helper on Linuxbrew

--- microagent-kit.rb
+++ microagent-kit.rb
@@ -9,35 +9,37 @@ class MicroagentKit < Formula
       revision: "3cc86425f15352649ce231deee9be930d182f0dd"
 
   depends_on "go" => :build
-  depends_on xcode: :build
+  depends_on xcode: :build if OS.mac?
   depends_on "e2fsprogs"
 
   def install
     system "go", "build",
-      "-ldflags", "-s -w -X main.version=#{version}",
-      "-o", bin/"microagent",
-      "./cmd/microagent"
+           "-ldflags", "-s -w -X main.version=#{version}",
+           "-o", bin/"microagent",
+           "./cmd/microagent"
 
     with_env(GOOS: "linux", GOARCH: "arm64", CGO_ENABLED: "0") do
       system "go", "build",
-        "-ldflags", "-s -w",
-        "-o", libexec/"microagent-guestinit-arm64",
-        "./cmd/microagent-guestinit"
+             "-ldflags", "-s -w",
+             "-o", libexec/"microagent-guestinit-arm64",
+             "./cmd/microagent-guestinit"
     end
 
     with_env(GOOS: "linux", GOARCH: "amd64", CGO_ENABLED: "0") do
       system "go", "build",
-        "-ldflags", "-s -w",
-        "-o", libexec/"microagent-guestinit-amd64",
-        "./cmd/microagent-guestinit"
+             "-ldflags", "-s -w",
+             "-o", libexec/"microagent-guestinit-amd64",
+             "./cmd/microagent-guestinit"
     end
 
-    cd "helpers/applevf" do
-      system "swift", "build", "--configuration", "release", "--disable-sandbox"
-      system "codesign", "-s", "-", "-f",
-        "--entitlements", "microagent-applevf-helper.entitlements",
-        ".build/release/microagent-applevf-helper"
-      bin.install ".build/release/microagent-applevf-helper"
+    if OS.mac?
+      cd "helpers/applevf" do
+        system "swift", "build", "--configuration", "release", "--disable-sandbox"
+        system "codesign", "-s", "-", "-f",
+               "--entitlements", "microagent-applevf-helper.entitlements",
+               ".build/release/microagent-applevf-helper"
+        bin.install ".build/release/microagent-applevf-helper"
+      end
     end
   end
 
@@ -48,8 +50,10 @@ class MicroagentKit < Formula
     assert_path_exists libexec/"microagent-guestinit-arm64"
     assert_path_exists libexec/"microagent-guestinit-amd64"
 
-    output = pipe_output("#{bin}/microagent-applevf-helper", '{"command":"host"}', 0)
-    assert_match '"ok" : true', output
-    assert_match '"backend" : "apple-vf"', output
+    if OS.mac?
+      output = pipe_output("#{bin}/microagent-applevf-helper", '{"command":"host"}', 0)
+      assert_match '"ok" : true', output
+      assert_match '"backend" : "apple-vf"', output
+    end
   end
 end
