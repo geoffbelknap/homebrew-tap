f7899b72ca0b478360ece8f26397b679dbda495a
Geoff Belknap <geoffbelknap@users.noreply.github.com>
2026-05-01 21:47:59 -0700

    Update microagent kit formula

--- microagent-kit.rb
+++ microagent-kit.rb
@@ -5,11 +5,12 @@ class MicroagentKit < Formula
   desc "CLI and helper protocol for agent microVMs"
   homepage "https://github.com/geoffbelknap/microagent-kit"
   url "https://github.com/geoffbelknap/microagent-kit.git",
-      tag:      "v0.1.0",
-      revision: "4a1e73688ce8b8051c700704c7bf0138ea3281da"
+      tag:      "v0.1.1",
+      revision: "3cffbdb19d1749595565631cffaf818e7037da1f"
 
   depends_on "go" => :build
   depends_on xcode: :build
+  depends_on "e2fsprogs"
 
   def install
     system "go", "build",
@@ -25,6 +26,7 @@ class MicroagentKit < Formula
 
   test do
     assert_match "microagent #{version}", shell_output("#{bin}/microagent version")
+    assert_match "image_ref is required", shell_output("#{bin}/microagent rootfs build 2>&1", 1)
 
     output = pipe_output("#{bin}/microagent-applevf-helper", '{"command":"host"}', 0)
     assert_match '"ok" : true', output
