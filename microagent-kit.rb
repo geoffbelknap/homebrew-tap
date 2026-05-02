07dff87e2f3f68146de3a1214aa5177a1f30e2fa
geoffbelknap <geoffbelknap@users.noreply.github.com>
2026-05-02 18:41:03 +0000

    Update microagent-kit to v0.1.22

--- microagent-kit.rb
+++ microagent-kit.rb
@@ -5,8 +5,8 @@ class MicroagentKit < Formula
   desc "Run Linux workspaces inside microVMs"
   homepage "https://github.com/geoffbelknap/microagent-kit"
   url "https://github.com/geoffbelknap/microagent-kit.git",
-      tag:      "v0.1.21",
-      revision: "16f636ea2c8ad0450f82d84974e62325445eb890"
+      tag:      "v0.1.22",
+      revision: "47a764c68b1e73add2f8b55589f37de607f20a91"
 
   depends_on "go" => :build
   depends_on xcode: :build if OS.mac?
