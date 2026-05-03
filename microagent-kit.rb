26b30584e332e93add314db57418dfa7fbd19cd2
Geoff Belknap <geoffbelknap@users.noreply.github.com>
2026-05-03 13:18:29 -0700

    Package default microagent kernels

--- microagent-kit.rb
+++ microagent-kit.rb
@@ -5,12 +5,21 @@ class MicroagentKit < Formula
   desc "Run Linux workspaces inside microVMs"
   homepage "https://github.com/geoffbelknap/microagent-kit"
   url "https://github.com/geoffbelknap/microagent-kit.git",
-      tag:      "v0.1.30",
-      revision: "a57c90ba6028ededb42d5e8cf287f22f522a2c93"
+      tag:      "v0.1.31",
+      revision: "5f1f5790de0aa9fc2d5e280059d474a605bc3930"
 
   depends_on "go" => :build
   depends_on xcode: :build if OS.mac?
   depends_on "e2fsprogs"
+
+  on_macos do
+    on_arm do
+      resource "apple-vf-kernel" do
+        url "https://github.com/geoffbelknap/microagent-kit/releases/download/kernels-6.12.22-r1/microagent-kernel-6.12.22-apple-vf-arm64"
+        sha256 "73fe78e51a8ce348e69311d376a02114440eee6b60bf2e91af54bdf2dfb405ec"
+      end
+    end
+  end
 
   on_linux do
     on_arm do
@@ -24,6 +33,11 @@ class MicroagentKit < Formula
       resource "firecracker" do
         url "https://github.com/firecracker-microvm/firecracker/releases/download/v1.15.1/firecracker-v1.15.1-x86_64.tgz"
         sha256 "d4a32ab2322d887ca1bc4a4e7afa9cc35393e6362dfc2b3becb389d362e4275a"
+      end
+
+      resource "firecracker-kernel" do
+        url "https://github.com/geoffbelknap/microagent-kernels/releases/download/kernels-6.1.155-r2/microagent-kernel-6.1.155-firecracker-amd64"
+        sha256 "4bbe8b2fd19f78fea4bf02d52a67482227a896c90a63f272b6a084fa46a416c0"
       end
     end
   end
@@ -56,23 +70,27 @@ class MicroagentKit < Formula
                ".build/release/microagent-applevf-supervisor"
         bin.install ".build/release/microagent-applevf-supervisor"
       end
+      if Hardware::CPU.arm?
+        resource("apple-vf-kernel").stage do
+          (libexec/"kernels/apple-vf/arm64").install "microagent-kernel-6.12.22-apple-vf-arm64" => "Image"
+        end
+      end
     else
       firecracker_arch = Hardware::CPU.arm? ? "aarch64" : "x86_64"
       resource("firecracker").stage do
         libexec.install "firecracker-v1.15.1-#{firecracker_arch}" => "firecracker"
       end
-    end
-  end
-
-  def post_install
-    if (OS.mac? && Hardware::CPU.arm?) || (OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?)
-      system bin/"microagent", "kernel", "install"
+      if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+        resource("firecracker-kernel").stage do
+          (libexec/"kernels/firecracker/amd64").install "microagent-kernel-6.1.155-firecracker-amd64" => "Image"
+        end
+      end
     end
   end
 
   def caveats
     <<~EOS
-      Microagent installs its default kernel during Homebrew post-install on supported hosts:
+      Microagent includes its default kernel on supported hosts:
         - macOS arm64: Apple Virtualization Framework kernel
         - Linux x86_64: Firecracker kernel
 
