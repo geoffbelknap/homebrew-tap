1587afb51a62d4b8fefa8a89198d35bb408d85e9
Geoff Belknap <geoffbelknap@users.noreply.github.com>
2026-05-10 14:41:26 -0700

    microagent-kit 0.1.34

--- microagent-kit.rb
+++ microagent-kit.rb
@@ -5,13 +5,8 @@ class MicroagentKit < Formula
   desc "Run Linux workspaces inside microVMs"
   homepage "https://github.com/geoffbelknap/microagent-kit"
   url "https://github.com/geoffbelknap/microagent-kit.git",
-      revision: "c74d20abf6c8f45c0a0978b08886e2fefab474bf"
-  version "0.1.33"
-
-  bottle do
-    root_url "https://github.com/geoffbelknap/homebrew-tap/releases/download/microagent-kit-0.1.33"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "0270e6213e888d2d9a68d0d3afc479f535f69311823a5c6070fd1635567f1999"
-  end
+      revision: "41059fb59c4435d922e570e46ac795c7a1abce42"
+  version "0.1.34"
 
   depends_on "go" => :build
   depends_on xcode: :build if OS.mac?
