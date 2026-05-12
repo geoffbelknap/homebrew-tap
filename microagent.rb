8c68b82bee63375d18e44acc4126b0a173dbe395
Geoff Belknap <geoffbelknap@users.noreply.github.com>
2026-05-11 20:55:23 -0700

    Bump microagent to 0.1.40

--- microagent.rb
+++ microagent.rb
@@ -5,8 +5,8 @@ class Microagent < Formula
   desc "Run Linux workspaces inside microVMs"
   homepage "https://github.com/geoffbelknap/microagent"
   url "https://github.com/geoffbelknap/microagent.git",
-      revision: "81cbbd6956b20a57463392486a2bf93ec6bb1d83"
-  version "0.1.39"
+      revision: "b648e6dda0cf624bc4cea4fcd322568d87125654"
+  version "0.1.40"
 
   depends_on "go" => :build
   depends_on xcode: :build if OS.mac?
